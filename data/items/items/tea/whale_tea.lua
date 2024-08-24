dd5a1dc204af33fad7d0d3a1504762a70ee371c7
Liu Wenyuan <15816141883@163.com>
2024-08-24 21:59:38 +0800

    items: Work around teas crashing game on launch

---
+++ data/items/items/tea/whale_tea.lua
@@ -0,0 +1,68 @@
+local item, super = Class(HealItem, "whale_tea")
+
+function item:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "Whale Tea"
+    -- Name displayed when used in battle (optional)
+    self.use_name = nil
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "item"
+    -- Item icon (for equipment)
+    self.icon = nil
+
+    -- Battle description
+    self.effect = "Healing\nvaries"
+    -- Shop description
+    self.shop = ""
+    -- Menu description
+    self.description = "It's own-flavored tea.\nThe flavor just says \"Whale.\""
+    -- Amount that this item heals the owner
+    self.heal_amount = 10
+    -- Party member this tea is from
+    self.tea_self = "whale"
+
+    -- Default shop price (sell price is halved)
+    self.price = 10
+    -- Whether the item can be sold
+    self.can_sell = true
+
+    -- Consumable target mode (ally, party, enemy, enemies, or none)
+    self.target = "ally"
+    -- Where this item can be used (world, battle, all, or none)
+    self.usable_in = "all"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+    -- Will this item be instantly consumed in battles?
+    self.instant = false
+
+    -- Equip bonuses (for weapons and armor)
+    self.bonuses = {}
+    -- Bonus name and icon (displayed in equip menu)
+    self.bonus_name = nil
+    self.bonus_icon = nil
+
+    -- Equippable characters (default true for armors, false for weapons)
+    self.can_equip = {}
+
+    -- Character reactions (key = party member id)
+    self.reactions = {
+    }
+end
+
+function item:getHealAmount(id)
+    if id ~= self.tea_self then
+        local user = Game:getPartyMember(id)
+        return user:getOpinion(self.tea_self)
+    end
+    return self.heal_amount
+end
+
+function item:getBattleHealAmount(id)
+    -- Dont heal less than 40HP in battles
+    return math.max(40, super.getBattleHealAmount(self, id))
+end
+
+return item
